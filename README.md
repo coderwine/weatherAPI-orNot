f925d657cc9734744b5569f72d0dd4784b349675
Eric <ejwinebrenner@gmail.com>
2020-06-06 17:14:41 -0400

    6/6 notes

--- README.md
+++ README.md
@@ -8,24 +8,37 @@ General Notes:
             - May look into OAuth as another goal measure depending on how this project develops.
         - Track progression via README file per date status records.
         - Find another UX/UI template build (like bootstrap) to practice with.
+            - Found https://materializecss.com/ and I'm very interested in playing around with this more.
         - Update personal portfolio.
         - End of Project details of what was learned and what could have been done better.
 
     APIs Utilized:
         1) https://weatherstack.com (free versioning)
+        2) https://developers.teleport.org/ (considering)
+        3) https://ip-api.com/ (IP Address Location) (no API Key Req.)
+
+    Other Tools Utilized:
+        1) https://materializecss.com/
 
     QUICK NOTES
         - gitignore created to protect personal access keys.  In order to allow for these projects to work, a separate JS file was created to pull these keys from within the HTML.  
 
 ****
-Project Notes:
-    - Should include location id per browser.
-
-****
-Status Notes:
-6/5/20
-    - Weatherstack free versioning doesn't provide https.  Only available for standard plan (or higher).
-        - Got my fetch to work correctly and set the values to various points of interest that I will want to display.
-    
     TO DO:
         - Find additional APIs to inject.  
+            - Teleport - Quality of Life
+            - A quick Joke API ? 
+        - Start displaying fetched APIs on DOM.
+****
+Status Notes:
+2020
+6/5
+    - Weatherstack free versioning doesn't provide https.  Only available for standard plan (or higher).
+        - Got my fetch to work correctly and set the values to various points of interest that I will want to display.
+
+6/6
+    - Got link to ip-api to fetch properly.
+    - Did a quick layout utilizing martializecss.com.
+        - Focus on Mobile First.
+        - May utilize a different card to display different information (such as a tabbed card)
+
--- scripts/weatherStack.js
+++ scripts/weatherStack.js
@@ -7,37 +7,60 @@ const histURL = `https//api.weatherstack.com/historical?access_key=${weatherStac
 let currentLocation = 'Indianapolis'
 let degree = '&units=f'  //calls on unit of measurement
 
-function fetchWeatherStack(e) {
+function fetchWeatherStack(ip) {
     
     let buildURL = `${baseURL}${currentLocation}${degree}`;
+    let fetchIP = ip;
+
     // e.preventDefault();
     
     fetch(buildURL)
         .then(console.log(buildURL))
         .then(response => response.json())
         // .then(data => console.log(data))
-        .then(data => setItems(data))
+        .then(data => setItems(data, fetchIP))
         .catch((error) => {
-            console.log('Errors Suck: ', error);
+            console.log('fetchWeatherStack Errors: ', error);
         })
-
     
 }
 
-fetchWeatherStack()
+// fetchWeatherStack()
 
-function setItems(data) {
-    // console.log('Current: ', data.current)
-    console.log('Temp: ', data.current.temperature)
-    console.log('Humidity: ', data.current.humidity)
-    console.log('Precip: ', data.current.precip)
-    console.log('UV: ', data.current.uv_index)
-    console.log('Wind Speed: ', data.current.wind_speed)
-    console.log('Wind Direction: ', data.current.wind_dir)
-    console.log('Description: ', data.current.weather_descriptions[0])
-    console.log('icon: ', data.current.weather_icons[0])
+function ipFinder() {
+    let ipFind = `http://ip-api.com/json/`;
+    
+        fetch(ipFind)
+            .then(response => response.json())
+            .then(ipInfo => fetchWeatherStack(ipInfo))
+            .catch(error => console.log('ipFinder Errors: ', error));
+}
 
-    // console.log('Location: ', data.location)
-    console.log('City: ', data.location.name) //City
-    console.log('State: ', data.location.region) //State
+
+ipFinder();
+
+function setItems(weather, ip) {
+
+    let theWeather = weather;
+
+    console.log('Current: ', theWeather.current)
+    console.log('Temp: ', theWeather.current.temperature)
+    console.log('Humidity: ', theWeather.current.humidity)
+    console.log('Precip: ', theWeather.current.precip)
+    console.log('UV: ', theWeather.current.uv_index)
+    console.log('Wind Speed: ', theWeather.current.wind_speed)
+    console.log('Wind Direction: ', theWeather.current.wind_dir)
+    console.log('Description: ', theWeather.current.weather_descriptions[0])
+    console.log('icon: ', theWeather.current.weather_icons[0])
+
+    console.log('Location: ', theWeather.location)
+    console.log('City: ', theWeather.location.name) //City
+    console.log('State: ', theWeather.location.region) //State
+
+    console.log('IP FIND: ', data);
+    console.log('IP query: ', data.query);
+    console.log('IP Provider: ', data.org);
+    console.log('IP City: ', data.city);
+    console.log('IP State: ', data.region);
+    console.log('IP ISP: ', data.isp);
 }
